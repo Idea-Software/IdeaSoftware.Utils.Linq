2c882d4c9cec354622141a2a6f2b2534a056db7e
Sergej Popov <sergejpopov@outlook.com>
2015-03-20 20:09:00 +0000

    Added ForEach methods with indexes and other metadata

---
+++ IdeaSoftware.Utils.Linq.Tests/EnumerationsShould.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace IdeaSoftware.Utils.Linq.Tests
+{
+    [TestFixture]
+    public class EnumerationsShould
+    {
+        private readonly List<string> _someList = new List<string> { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j" };
+
+        [Test]
+        public void CallDelegateCorrectAmountOfTimes_OneEachWithIndex()
+        {
+            var count = 0;
+            _someList.Each((str, i) =>
+            {
+                count++;
+            });
+            count.Should().Be(_someList.Count);
+        }
+        [Test]
+        public void ProvideCorrectIndex_OneEachWithIndex()
+        {
+            var index = 0;
+            _someList.Each((str, i) =>
+            {
+                i.Should().Be(index);
+                index++;
+            });
+        }
+
+
+
+
+        [Test]
+        public void CallDelegateCorrectAmountOfTimes_OneEachWithMeta()
+        {
+            var count = 0;
+            _someList.Each(meta =>
+            {
+                count++;
+            });
+            count.Should().Be(_someList.Count);
+        }
+        [Test]
+        public void ProvideCorrectIndex_OneEachWithMeta()
+        {
+            var index = 0;
+            _someList.Each(meta =>
+            {
+                meta.Index.Should().Be(index);
+                index++;
+            });
+        }
+        [Test]
+        public void IndicateFirstItemCorrectly_OneEachWithMeta()
+        {
+            _someList.Each(meta =>
+            {
+                if (meta.Index == 0)
+                    meta.IsFirst.Should().BeTrue();
+                else
+                    meta.IsFirst.Should().BeFalse();
+            });
+        }
+        [Test]
+        public void IndicateLasItemCorrectly_OneEachWithMeta()
+        {
+            _someList.Each(meta =>
+            {
+                if (meta.Index == _someList.Count - 1)
+                    meta.IsLast.Should().BeTrue();
+                else
+                    meta.IsLast.Should().BeFalse();
+            });
+        }
+
+    }
+}
---
+++ IdeaSoftware.Utils.Linq/Enumerations.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IdeaSoftware.Utils.Linq
+{
+    public static class Enumerations
+    {
+        /// <summary>
+        /// Provides index when looping through collection
+        /// </summary>
+        /// <param name="collection">Collection to enumerate.</param>
+        /// <param name="action">Delegate to call for each item.</param>
+        public static void Each<T>(this IEnumerable<T> collection, Action<T, int> action)
+        {
+            var index = 0;
+            foreach (var item in collection)
+            {
+                action(item, index);
+                index++;
+            }
+        }
+
+        /// <summary>
+        /// Enumerated collection providing iteration metadata.
+        /// </summary>
+        /// <param name="collection">Collection to enumerate.</param>
+        /// <param name="action">Delegate to call for each item.</param>
+        public static void Each<T>(this IEnumerable<T> collection, Action<Item<T>> action)
+        {
+            var enumeratedCollection = collection.ToList();
+            var index = 0;
+            foreach (var item in enumeratedCollection)
+            {
+                action(new Item<T>
+                {
+                    Value = item,
+                    Index = index,
+                    IsFirst = index == 0,
+                    IsLast = index == enumeratedCollection.Count - 1
+                });
+                index++;
+            }
+        }
+        public class Item<T>
+        {
+            public T Value { get; set; }
+            public int Index { get; internal set; }
+            public bool IsFirst { get; internal set; }
+            public bool IsLast { get; internal set; }
+        }
+    }
+
+}
